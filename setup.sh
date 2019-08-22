474f8301ca5bd7476f3fb3ff79be2f64c04eb2cc
James Bruska <jamesbruska@hotmail.com>
2019-08-22 08:51:00 -0700

    updated setup file

--- setup.sh
+++ setup.sh
@@ -1,3 +1,8 @@
 #! /bin/bash
 
+sudo apt install curl -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
+cp .vimrc ~/.vimrc
+
+sudo apt install i3 -y
+cp config ~/.config/i3/config
