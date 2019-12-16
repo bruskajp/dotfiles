498cb2889d677853603ac51715dbd2ed8cbed1e0
James Bruska <jamesbthecoder@gmail.com>
2019-12-16 05:54:18 -0800

    added instructions in setup.sh

--- setup.sh
+++ setup.sh
@@ -3,6 +3,8 @@
 sudo apt install curl -y
 curl -fLo ~/.vim/autoload/plug.vim --create-dirs https://raw.githubusercontent.com/junegunn/vim-plug/master/plug.vim
 cp .vimrc ~/.vimrc
+# You need to run ":PlugInstall" in vim
 
 sudo apt install i3 -y
+# Run i3 before you run this command
 cp config ~/.config/i3/config
